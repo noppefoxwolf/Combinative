1c43c31115b87c9b9b63c7f07951d9851f4b12a2
🦊Tomoya Hirano <tomoya.hirano@dena.com>
2019-07-08 21:11:54 +0900

    Add Example

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Tomoya Hirano on 2019/07/08.
+//  Copyright © 2019 Tomoya Hirano. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Tomoya Hirano on 2019/07/08.
+//  Copyright © 2019 Tomoya Hirano. All rights reserved.
+//
+
+import UIKit
+import Combinative
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
